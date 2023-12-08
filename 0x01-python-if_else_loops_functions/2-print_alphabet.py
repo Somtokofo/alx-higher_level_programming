d475685e1145cf6456f3795f2bafeff00628dc72
Somtokofo <skofojr@gmail.com>
2023-12-08 00:09:35 -0500

    correcting errors

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for char in range(ord('a'), ord('z') + 1):
-    print(chr(char), end='')
+"""Print the alphabet in lowercase, not followed by a new line."""
+
+for letter in range(97, 123):
+	print("{}".format(chr(letter)), end="")
